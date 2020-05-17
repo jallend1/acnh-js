a52afc45046377d7e0d7a7c999ec3073c7192d9f
jallend1 <jallend1@gmail.com>
2020-05-17 15:47:19 -0700

    Adds collapsible divs for item details, and click events to trigger

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const types = document.querySelector('#types');
 const fishList = document.querySelector('#fishlist');
 const endpoint = './fish.json';
 
@@ -20,15 +21,33 @@ const displayFish = (fishies) => {
         const fishMuseum = (fish["museum-phrase"]);
         const newFish = document.createElement('div');
         newFish.innerHTML = `
-            <h3>${fishName}</h3>
-            <h4>${fishPhrase}</h4>
-            <p>${fishMuseum}</p>
-            <p class="price">Value: ${fishPrice} </p>
+            <div>
+                <header>
+                    <h3>${fishName}</h3>
+                    <h3 class="price"> ${fishPrice} bells </h3>
+                    <img class="toggle" src="./images/arrow-expand.png">
+                </header>
+                <main class="collapsed">
+                    <h4>${fishPhrase}</h4>
+                    <p>${fishMuseum}</p>
+                </main>
+            </div>
             `;
         newFish.classList.add('fish');
-
         fishList.appendChild(newFish);
     })
 }
 
+const expandFish = e => {
+    if(e.target.classList.contains('toggle')){                              // Limits collapsing to just the item divs
+        const details = e.target.parentNode.parentNode.children[1];         // Selects the div with the deets
+        details.classList.toggle('collapsed')
+        details.classList.contains('collapsed') 
+            ? e.target.src = './images/arrow-expand.png' 
+            : e.target.src = './images/arrow-collapse.png';
+    }
+}
+
 catchFish();
+
+fishList.addEventListener('click', expandFish);
